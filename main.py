81baca364ba45104d904c049228d7dcf6d9931de
povilas taraikavicius <ptpecka@gmail.com>
2023-10-12 00:31:25 +0300

    add program

--- main.py
+++ main.py
@@ -4,6 +4,38 @@
 # check ff credentials are correct stop endless loop and print greeting message, otherwise print 'wrong credentials'
 # and ask to enter password and username again. The program should show numbers of times you tried to enter both credentials.
 
+# print("Enter five different username and password")
+# name_and_pasvord =input ("username1=": "password1","username2=": "password2","username3=": "password3","username4=": "password4","username5=": "password5")
+# print(name_and_pasvord)
+
+
+
+
+
+user_name_list = []
+user_password_list = []
+
 print("Enter five different username and password")
-Name_and_pasword = input("username='', password=''";"username='', password=''";"username='', password=''";"username='', password=''";"username='', password=''")
-print(Name_and_pasword)
+
+for add in range(5):
+    username = input("Add username: ")
+    password = input("Add password: ")
+    user_name_list.append(f"username='{username}'")
+    user_password_list.append(f"password='{password}'")
+
+while True:
+    username_input = input("Enter username: ")
+    password_input = input("Enter password: ")
+
+    if f"username='{username_input}'" in user_name_list and f"password='{password_input}'" in user_password_list:
+        print("Welcome")
+        break
+    else:
+        print("Wrong password. Please try again.")
+
+        
+
+    
+
+    
+
