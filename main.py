422862e5d87f28b7e55855b5d9416476b0e634af
povilas taraikavicius <ptpecka@gmail.com>
2023-10-11 19:40:49 +0300

    add program 23-10-11

---
+++ main.py
@@ -0,0 +1,9 @@
+#   Create a program witch takes 5 different (minmum)  variables containing strings for username and password.
+# The string should look like : "username='', password=''; username='',password='';....etc"
+# Start Endless loop allowing to enter username and password and 
+# check ff credentials are correct stop endless loop and print greeting message, otherwise print 'wrong credentials'
+# and ask to enter password and username again. The program should show numbers of times you tried to enter both credentials.
+
+print("Enter five different username and password")
+Name_and_pasword = input("username='', password=''";"username='', password=''";"username='', password=''";"username='', password=''";"username='', password=''")
+print(Name_and_pasword)
